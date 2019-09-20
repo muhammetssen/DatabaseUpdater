51094e1f41f597107a0ebe1284ba0b86d6b5e223
Muhammet <enmuhammet23@gmail.com>
2019-09-20 08:34:15 +0300

    Fixed Youtube function and created Twitter Function.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pandas
-import numpy
-inf_sheet = pandas.read_excel('excel.xlsx',sheet_name='Influencers',skiprows=[0])
-media_sheet = pandas.read_excel('excel.xlsx',sheet_name='Media Contacts',skiprows=[0])
+inf_sheet = pandas.read_excel('excel.xlsx',sheet_name='Influencers')
+media_sheet = pandas.read_excel('excel.xlsx',sheet_name='Media Contacts')
 
+if inf_sheet.columns[0] != 'Influencer Name':
+    inf_sheet = pandas.read_excel('excel.xlsx',sheet_name='Influencers',skiprows=[0])
 
+if media_sheet.columns[0] != 'Contact Name':
+    media_sheet = pandas.read_excel('excel.xlsx',sheet_name='Media Contacts',skiprows=[0])
+    
 
 
 def save():
@@ -13,18 +17,19 @@ def save():
        'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18',
        'Unnamed: 19', 'Twitter', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23',
        'Unnamed: 24', 'Unnamed: 25', 'Unnamed: 26', 'Facebook', 'Unnamed: 28']
-    '''media_sheet.loc[-1] = header
+    '''media_sheet.loc[-1] = pandas.read_excel('excel.xlsx',sheet_name='Media Contacts').columns
     media_sheet.index +=1
     media_sheet.sort_index()'''
 
-    inf_sheet.loc[-1] = header
+    '''inf_sheet.loc[-1] = pandas.read_excel('excel.xlsx',sheet_name='Influencers').columns
     inf_sheet.index +=1
     inf_sheet.sort_index()
-    
+    '''
     writer = pandas.ExcelWriter('output.xlsx', engine='xlsxwriter')
     media_sheet.to_excel(writer,sheet_name='Media Contact')
     inf_sheet.to_excel(writer,sheet_name='Influencers')
     writer.save()
+    return True
 
 
 names = inf_sheet['Influencer Name']
@@ -45,15 +50,42 @@ for person in names: # Main Loop
         #else:
          #   person_information[str(column)]='n/a'
     #inf_sheet.loc[inf_sheet['Influencer Name']] == str(person)
+
+
     youtube_id = person_information['Youtube Link']
-    print(youtube_id)
     if not pandas.isna(youtube_id):
         from socialblade import get_youtube_info
         channel_name = youtube_id.split('/')[4]
+        print('Youtube Channel Name: {}'.format(channel_name))
         data = get_youtube_info(channel_name)
-        person_information['Uploads'] = data['youtube-stats-header-uploads']
-        person_information['Subs'] = data['youtube-stats-header-subs']
-        person_information['Views'] = data['youtube-stats-header-views']
+        if data != 'failed':
+            person_information['Uploads'] = data['youtube-stats-header-uploads']
+            person_information['Subscribers'] = data['youtube-stats-header-subs']
+            person_information['Video Views'] = data['youtube-stats-header-views']
+
+    
+    twitter_link = person_information['Twitter Link']
+    if not pandas.isna(twitter_link):
+        #print('twitter_link : {}'.format(twitter_link))
+        words = str(twitter_link).split('/') 
+        if 'twitter.com' in words:
+            ind = words.index('twitter.com')
+        if 'www.twitter.com' in words:
+            ind = words.index('www.twitter.com')
+        username = words[ind+1]
+        print('username: {}'.format(username))
+        from socialblade import get_twitter_info
+        data = get_twitter_info(username)
+        if data != 'failed':
+            index = columns.index('Twitter Link')
+            person_information[columns[index+1]] = data['Followers']
+            person_information[columns[index+2]] = data['Following']
+            person_information[columns[index+3]]= data['Likes']
+            person_information[columns[index+4]]= data['Tweets']
+            
+    
+    #print(person_information)
+    
     overwrite(inf_sheet,person_information,str(person))
-print(inf_sheet.head())
+
 save()
--- socialblade.py
+++ socialblade.py
@@ -1,34 +1,59 @@
-def get_youtube_info(channel_name):
-    import os  
-    from selenium import webdriver  
-    from selenium.webdriver.common.keys import Keys  
-    from selenium.webdriver.chrome.options import Options  
-    from bs4 import BeautifulSoup
-    chrome_options = Options()  
-    #chrome_options.add_argument("--headless")  
-    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os  
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.chrome.options import Options  
+from bs4 import BeautifulSoup
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "normal"  
 
-    caps = DesiredCapabilities().CHROME
-    caps["pageLoadStrategy"] = "normal"  
+def get_youtube_info(channel_name):
     driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), desired_capabilities=caps,  chrome_options=chrome_options)  
-    url = "https://socialblade.com/youtube/user/"+ str(channel_name)
-    print('Getting html')
+    url = "https://socialblade.com/youtube/channel/"+ str(channel_name)
     driver.get(url)
     html = driver.page_source
     driver.close()
+    try:
+        soup = BeautifulSoup(html,"html.parser")
+        div_block = soup.find_all("div",{"id":"YouTubeUserTopInfoBlockTop"})
+        spans = div_block[0].find_all("span")
+        spans_wanted_id = [
+            "youtube-stats-header-uploads",
+            "youtube-stats-header-subs",
+            "youtube-stats-header-views"    ]
+        contexts = {}
+        for span in spans:
+            if span.get("id") in spans_wanted_id:
+                contexts[span.get('id')] = span.contents[0]
+        print (contexts)
+        return contexts
+    except Exception as e:
+        print(e)
+        print('Couldn\'t update the channel information of {}. Please check your connection.'.format(channel_name))
+        return "failed"
 
-    soup = BeautifulSoup(html,"html.parser")
-    div_block = soup.find_all("div",{"id":"YouTubeUserTopInfoBlockTop"})
-    spans = div_block[0].find_all("span")
-    spans_wanted_id = [
-        "youtube-stats-header-uploads",
-        "youtube-stats-header-subs",
-        "youtube-stats-header-views"    ]
-    contexts = {}
-    for span in spans:
-        if span.get("id") in spans_wanted_id:
-            #contexts.append(span.contents[0])
-            contexts[span.get('id')] = span.contents[0]
-    print (contexts)
-    return contexts
+
+def get_twitter_info(username):
+        driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), desired_capabilities=caps,  chrome_options=chrome_options)  
+        url = 'https://socialblade.com/twitter/user/' + str(username)
+        driver.get(url)
+        html = driver.page_source
+        driver.close()
+        soup = BeautifulSoup(html,'html.parser')
+        #print(soup)
+        #print('lala\n\n\n',soup.find_all('div',{'id':'YouTubeUserTopInfoBlock'})[0].find_all('span'))
+        try:
+            contexts = {
+                'Followers': soup.find_all('div',{'id':'YouTubeUserTopInfoBlock'})[0].find_all('span')[2].contents[0],
+                'Following': soup.find_all('div',{'id':'YouTubeUserTopInfoBlock'})[0].find_all('span')[4].contents[0],
+                'Likes': soup.find_all('div',{'id':'YouTubeUserTopInfoBlock'})[0].find_all('span')[6].contents[0],
+                'Tweets': soup.find_all('div',{'id':'YouTubeUserTopInfoBlock'})[0].find_all('span')[8].contents[0],
+            }
+            print(contexts)
+            return contexts
+        except Exception as e:
+            print('Error: {}\nCouldn\'t update the twitter information of {}'.format(e,username))
+            return 'failed'
 
